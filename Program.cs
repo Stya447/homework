e4169ce445cf3b3772d98eda2dc4dcd3b8daba95
Anastasia Akhmetova <asyaphilip447@gmail.com>
2023-10-15 17:57:02 +0300

    Написана первая задача

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+//int NumberSearch (int num)
+//{
+//    if (num >= 100)
+//    {
+//    int newnum = (num % 100) / 10;
+//    return newnum;
+//    } else {
+//        Console.WriteLine ("Введите, пожалуйста, трехзначное число"); return 0; }
+//}
+//Console.WriteLine ("Введите трехзначное число: ");
+//int num = Convert.ToInt32 (Console.ReadLine());
+//Console.WriteLine (NumberSearch(num));
+
+//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
