349e7bd28f9c01e751ee0fe452d935358ccc2f09
Anastasia Akhmetova <asyaphilip447@gmail.com>
2023-10-15 18:29:07 +0300

    Решение третьей задачи

--- Program.cs
+++ Program.cs
@@ -21,16 +21,31 @@
 //78 -> третьей цифры нет
 //32679 -> 6
 
-int NumberSearch (int num)
+//int NumberSearch (int num)
+//{
+//    int newnum = 0;
+//    if ((num >= 100) && (num < 1000)) newnum = num % 10;
+//    else if ((num >= 1000) && (num < 10000)) newnum = (num % 100)/10;
+//    else if ((num >= 10000) && (num < 100000)) newnum = (num % 1000)/100;
+//    else if (num < 100) Console.WriteLine ("Третьей цифры нет");
+//    else Console.WriteLine ("Введите, пожалуйста, число в диапозоне от 100 до 100000");  
+//    return newnum;
+//}
+//Console.WriteLine ("Введите число: ");
+//int num = Convert.ToInt32 (Console.ReadLine());
+//Console.WriteLine (NumberSearch(num));
+
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+void DayOfTheWeek (int num)
 {
-    int newnum = 0;
-    if ((num >= 100) && (num < 1000)) newnum = num % 10;
-    else if ((num >= 1000) && (num < 10000)) newnum = (num % 100)/10;
-    else if ((num >= 10000) && (num < 100000)) newnum = (num % 1000)/100;
-    else if (num < 100) Console.WriteLine ("Третьей цифры нет");
-    else Console.WriteLine ("Введите, пожалуйста, число в диапозоне от 100 до 100000");  
-    return newnum;
+    if ((num == 6) || (num == 7))
+    Console.WriteLine ($"{num} -> да"); 
+    else Console.WriteLine ($"{num} -> нет"); 
 }
-Console.WriteLine ("Введите число: ");
+Console.WriteLine ("Введите число, я проверю, являеется ли оно выходным: ");
 int num = Convert.ToInt32 (Console.ReadLine());
-Console.WriteLine (NumberSearch(num));
+DayOfTheWeek(num);
